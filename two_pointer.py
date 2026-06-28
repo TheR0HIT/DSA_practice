ba9388e2e414d0389fc6ccc4f4353c11c2dcd882
TheR0HIT <rohitrajsingh973@gmail.com>
2026-06-28 23:24:51 +0530

    add two pointer solutions

---
+++ two_pointer.py
@@ -0,0 +1,9 @@
+def sorted(self,arr):
+    left,right=0,len(arr)-1
+    while left<right:
+        arr[left]**2
+        arr[right]**2
+        if arr[left]<arr[right]:
+            return arr[left]
+        else:
+            return arr[right]
